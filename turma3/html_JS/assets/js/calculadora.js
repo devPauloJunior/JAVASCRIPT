38891034c20e9212708e61a525b600142b6c1b52
Paulo Junior <npaulo.ce@gmail.com>
2024-01-23 19:39:50 -0300

    Aula HTML+JS calculadora parte 3 23/01

--- turma3/html_JS/assets/js/calculadora.js
+++ turma3/html_JS/assets/js/calculadora.js
@@ -1,3 +1,16 @@
 function calcular(tipo, valor) {
-    console.log(`O tipo é: ${tipo} e o valor é: ${valor}`)
+    if ( tipo === 'num' ) {
+        document.getElementById('resultado').value += valor
+    } else if (tipo === 'acao') {
+        if ( valor === 'c' ) {
+            document.getElementById('resultado').value =  ''
+        }
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            document.getElementById('resultado').value += valor
+        }
+        if (valor === '=') {
+            let valorCampo = eval(document.getElementById('resultado').value)
+            document.getElementById('resultado').value = valorCampo
+        }
+    }
 }
